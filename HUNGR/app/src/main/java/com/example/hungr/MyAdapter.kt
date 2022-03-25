8480506ce3ae2434e9e1203eda0b507f575d3f69
Foster <fostemi>
2022-03-25 17:59:22 -0400

    ability to navigate pages

---
+++ HUNGR/app/src/main/java/com/example/hungr/MyAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.hungr
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+@Suppress("DEPRECATION")
+internal class MyAdapter(
+    var context: Context,
+    supportFragmentManager: FragmentManager,
+    var totalTabs: Int
+):
+    FragmentPagerAdapter(supportFragmentManager) {
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> {
+                FoodBank()
+            }
+            1 -> {
+                Volunteering()
+            }
+            else -> getItem(position)
+        }
+    }
+
+    override fun getCount(): Int {
+        return totalTabs
+    }
+}
